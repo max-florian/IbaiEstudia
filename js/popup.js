bb4a772bffbb1384976c53f7dcc3a3d35ed24b90
Max Florian <maxgt734@gmail.com>
2020-11-17 18:01:24 -0600

    El estado de modoEstudio se guarda en la extensión

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,11 @@ let imgTwitter = document.getElementById('imgTwitter');
 let aSourceCode = document.getElementById('aSourceCode');
 let btnModo = document.getElementById("btnModo");
 
+// Funcion para asignarle el valor del modoEstudio almacenado en memoria al toggle al iniciar la extensión
+chrome.storage.sync.get('modoestudio', function(data) {
+    btnModo.checked = data.modoestudio;
+});
+
 imgTwitter.onclick = function() {
     chrome.tabs.create({url: 'https://twitter.com/Max7Droid', active: false});
 };
@@ -10,10 +15,11 @@ aSourceCode.onclick = function() {
     chrome.tabs.create({url: 'https://github.com/K7Droid/IbaiEstudia', active: false});
 };
 
+//Funcion para almacenar en memoria el valor/estado del modoEstudio
 btnModo.onchange = function(){
     var activo = btnModo.checked;
     chrome.storage.sync.set({'modoestudio': activo}, function() {
-        //alert("Modo Estudio: "+activo);
+        console.log("Modo Estudio: "+activo);
     });
     
 };
