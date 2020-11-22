37a78cbf93c81d33980d6ee8ad43fe624ab9df6f
Max Florian <maxgt734@gmail.com>
2020-11-21 19:43:49 -0600

    Se agregó video de ibai

--- content.js
+++ content.js
@@ -1,13 +1,18 @@
 
+const videoURL = chrome.runtime.getURL("images/quemirasamigo.mp4");
+
 chrome.storage.sync.get('modoestudio', function(data) {
     if(data.modoestudio){
         newHTML =   `<html>
-                    <head>
-                        <title>A estudiar crack!</title>
-                    </head>
-                    <body>
-                        <p>Ibai te manda a estudiar</p>
-                    </body>
+                        <head>
+                            <title>A estudiar crack!</title>
+                        </head>
+                        <body>
+                            <video id="ibaivideo" width="100%" height="auto" controls autoplay>
+                                <source src="${videoURL}" type="video/mp4">
+                            </video>
+                            <script> document.getElementById('ibaivideo').play();  </script>
+                        </body>
                     </html>`
 
         document.open()
