7e8813733636926280cf694d6e301295e1735f83
Max Florian <maxgt734@gmail.com>
2020-11-22 19:32:28 -0600

    Se despliega el video cuando se pasa el mouse sobre las paginas "bloqueadas". Aun no se logra el funcionamiento deseado

--- content.js
+++ content.js
@@ -1,22 +1,32 @@
 
 const videoURL = chrome.runtime.getURL("images/quemirasamigo.mp4");
 
-chrome.storage.sync.get('modoestudio', function(data) {
-    if(data.modoestudio){
-        newHTML =   `<html>
-                        <head>
-                            <title>A estudiar crack!</title>
-                        </head>
-                        <body>
-                            <video id="ibaivideo" width="100%" height="auto" controls autoplay>
-                                <source src="${videoURL}" type="video/mp4">
-                            </video>
-                            <script> document.getElementById('ibaivideo').play();  </script>
-                        </body>
-                    </html>`
+document.body.addEventListener('mouseover',function() { reproducirVideo(); },false);
 
-        document.open()
-        document.write(newHTML)
-        document.close()
-    }
-});
+function reproducirVideo(){
+    chrome.storage.sync.get('modoestudio', function(data) {
+    
+        if(data.modoestudio){
+            newHTML =   `<html>
+                            <head>
+                                <title>A estudiar crack!</title>
+                            </head>
+                            <body>
+                                <video id="ibaivideo" width="100%" height="auto" controls>
+                                    <source src="${videoURL}" type="video/mp4">
+                                </video>
+                                <script> 
+                                    document.body.addEventListener('mouseover',function() { document.getElementById('ibaivideo').play(); },false);
+                                      
+                                </script>
+                            </body>
+                        </html>`
+    
+            document.open()
+            document.write(newHTML)
+            document.close()
+        }
+    });
+}
+
+
