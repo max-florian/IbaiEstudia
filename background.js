36b7dec515d4e04741b696ed7d03ce9165d0be58
Max Florian <maxgt734@gmail.com>
2021-01-03 19:38:15 -0600

    Se solucionaron problemas de permisos para reproducir el video automaticamente con sonido

---
+++ background.js
@@ -0,0 +1,35 @@
+sitiosbloqueados = [
+    new RegExp('https:\/\/www\.facebook\.com\/.*'),
+    new RegExp('https:\/\/www\.instagram\.com\/.*'),
+    new RegExp('https:\/\/twitter\.com\/.*'),
+    new RegExp('https:\/\/www\.youtube\.com\/.*'),
+    new RegExp('https:\/\/www\.reddit\.com\/.*'),
+    new RegExp('https:\/\/www\.tiktok\.com\/.*'),
+    new RegExp('https:\/\/www\.twitch.com\/.*')
+];
+function coincidencia(sitio){
+    for(var i=0;i<sitiosbloqueados.length;i++){
+        if(sitiosbloqueados[i].test(sitio)){
+            return true
+        }
+    }
+    return false
+}
+
+chrome.tabs.onActivated.addListener( function(activeInfo){
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        y = coincidencia(tab.url);
+        if(y){
+            chrome.tabs.update({ url: "chrome://bookmarks" })
+        }
+    });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+        y = coincidencia(change.url);
+        if(y){
+            chrome.tabs.update({ url: "chrome://bookmarks" })
+        }           
+    }
+});
--- js/popup.js
+++ js/popup.js
@@ -23,3 +23,4 @@ btnModo.onchange = function(){
     });
     
 };
+
