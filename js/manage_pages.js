df359c72b19718c4288de175e5eedea5387c94dd
Max <maxgt734@gmail.com>
2022-02-05 15:56:54 -0600

    Se implementaron los botones de eliminar y modificar de cada fila pero sin función atm

--- js/manage_pages.js
+++ js/manage_pages.js
@@ -12,6 +12,26 @@ chrome.storage.sync.get('sitiosbloqueados', function(data) {
         let video = row.insertCell(2);
         video.innerHTML = 'Videin';
         let accion = row.insertCell(3);
-        accion.innerHTML = '<button type="button" class="btn btn-outline-secondary"><i class="bi bi-wrench-adjustable"></i></button> <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>'
+        accion.innerHTML = '<a name="btnModificar" class="btn btn-outline-secondary"><i class="bi bi-wrench-adjustable"></i></a> <a name="btnEliminar" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></a>'
+
+        // OnClick event del boton de modificar:
+        accion.childNodes[0].onclick = function(){
+            alert("indice: "+(accion.parentNode.rowIndex-1))
+        }
+
+        // OnClick event del boton de eliminar:
+        accion.childNodes[2].onclick = function(){
+            alert("indice: "+(accion.parentNode.rowIndex-1))
+        }
+
+        
     });
+
+    
+    
+
 });
+
+
+
+
