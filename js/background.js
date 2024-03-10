53f71abbbf89464f76100ffd31e7860af7636564
Max Florian <maxgt734@gmail.com>
2024-03-10 17:30:12 -0600

    Se actualizo el codigo para que sea compatible con Manifest V3

--- js/background.js
+++ js/background.js
@@ -1,15 +1,62 @@
-function coincidencia(sitio,sitios){
-    for(let i=0;i<sitios.length;i++){
-        let regularexp = new RegExp(sitios[i]+".*");
-        if(regularexp.test(sitio)){
-            return true;
-        }
-    }
-    return false;
+// function coincidencia(sitio,sitios){
+//     for(let i=0;i<sitios.length;i++){
+//         let regularexp = new RegExp(sitios[i]+".*");
+//         if(regularexp.test(sitio)){
+//             return true;
+//         }
+//     }
+//     return false;
 
+// }
+
+// chrome.runtime.onInstalled.addListener( function(){
+//     let sitiosdefault = [
+//         'https:\/\/www\.facebook\.com\/',
+//         'https:\/\/www\.instagram\.com\/',
+//         'https:\/\/twitter\.com\/',
+//         'https:\/\/www\.youtube\.com\/',
+//         'https:\/\/www\.tiktok\.com\/',
+//         'https:\/\/www\.twitch.tv\/'
+
+//     ];
+//     chrome.storage.sync.set({'sitiosbloqueados': sitiosdefault}, function() {
+//     });
+//     alert("Empezar a usar la extensión: \n- Activa el modo estudio. \n\nAdministrar los sitios bloqueados: \n- Presiona el icono de ajustes.\n\n**IMPORTANTE**\nDependiendo del navegador que utilices es posible que debas reiniciar el navegador luego de agregar una nueva página web para guardar los cambios.");
+// });
+
+// chrome.tabs.onActivated.addListener( function(activeInfo){
+//     chrome.storage.sync.get(['modoestudio','sitiosbloqueados'], function(data) {
+//         if(data.modoestudio){
+//             chrome.tabs.get(activeInfo.tabId, function(tab){
+//                 y = coincidencia(tab.url,data.sitiosbloqueados);
+//                 if(y){
+//                     chrome.tabs.update({ url: "chrome://bookmarks" })
+//                     chrome.tabs.update({ url: "html/video.html" })
+//                 }
+//             });
+//         }
+//     });
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+//     chrome.storage.sync.get(['modoestudio','sitiosbloqueados'], function(data) {
+//         if(data.modoestudio){
+//             if (tab.active && change.url) {
+//                 y = coincidencia(change.url,data.sitiosbloqueados);
+//                 if(y){
+//                     chrome.tabs.update({ url: "chrome://bookmarks" })
+//                     chrome.tabs.update({ url: "html/video.html" })
+//                 }           
+//             }
+//         }
+//     });
+// });
+
+function coincidencia(sitio, sitios) {
+    return sitios.some(s => new RegExp(s + ".*").test(sitio));
 }
 
-chrome.runtime.onInstalled.addListener( function(){
+chrome.runtime.onInstalled.addListener(() => {
     let sitiosdefault = [
         'https:\/\/www\.facebook\.com\/',
         'https:\/\/www\.instagram\.com\/',
@@ -17,38 +64,38 @@ chrome.runtime.onInstalled.addListener( function(){
         'https:\/\/www\.youtube\.com\/',
         'https:\/\/www\.tiktok\.com\/',
         'https:\/\/www\.twitch.tv\/'
-
     ];
-    chrome.storage.sync.set({'sitiosbloqueados': sitiosdefault}, function() {
+    chrome.storage.sync.set({ 'sitiosbloqueados': sitiosdefault });
+
+    chrome.notifications.create({
+        type: 'basic',
+        iconUrl: '../images/ibai_icon48.png',
+        title: 'Comienza a usar la extensión',
+        message: 'Activa el modo estudio. \n\nAdministra los sitios bloqueados presionando el icono de ajustes.\n\nIMPORTANTE: Dependiendo del navegador, puede ser necesario reiniciar el navegador después de agregar una nueva página web para guardar los cambios.',
+        priority: 2
     });
-    alert("Empezar a usar la extensión: \n- Activa el modo estudio. \n\nAdministrar los sitios bloqueados: \n- Presiona el icono de ajustes.\n\n**IMPORTANTE**\nDependiendo del navegador que utilices es posible que debas reiniciar el navegador luego de agregar una nueva página web para guardar los cambios.");
+    
 });
 
-chrome.tabs.onActivated.addListener( function(activeInfo){
-    chrome.storage.sync.get(['modoestudio','sitiosbloqueados'], function(data) {
-        if(data.modoestudio){
-            chrome.tabs.get(activeInfo.tabId, function(tab){
-                y = coincidencia(tab.url,data.sitiosbloqueados);
-                if(y){
-                    chrome.tabs.update({ url: "chrome://bookmarks" })
-                    chrome.tabs.update({ url: "html/video.html" })
-                }
-            });
+function checkAndRedirect(tab) {
+    chrome.storage.sync.get(['modoestudio', 'sitiosbloqueados'], function(data) {
+        if (data.modoestudio && coincidencia(tab.url, data.sitiosbloqueados)) {
+            
+            chrome.tabs.update(tab.id, { url: "html/video.html" });
         }
+    });
+}
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+    chrome.tabs.get(activeInfo.tabId, tab => {
+        checkAndRedirect(tab);
     });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-    chrome.storage.sync.get(['modoestudio','sitiosbloqueados'], function(data) {
-        if(data.modoestudio){
-            if (tab.active && change.url) {
-                y = coincidencia(change.url,data.sitiosbloqueados);
-                if(y){
-                    chrome.tabs.update({ url: "chrome://bookmarks" })
-                    chrome.tabs.update({ url: "html/video.html" })
-                }           
-            }
-        }
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.active && changeInfo.url) {
+        checkAndRedirect(tab);
+    }
 });
+
 
--- js/manage_pages.js
+++ js/manage_pages.js
@@ -1,92 +1,84 @@
 const sitioform = document.getElementById('sitioform');
 
-sitioform.addEventListener('submit',()=>{
+sitioform.addEventListener('submit', () => {
     crear(sitioform.elements['inputUrl'].value);
 });
 
 chrome.storage.sync.get('sitiosbloqueados', function(data) {
     let cont = 0;
     const table = document.getElementById("pagestable");
-    data.sitiosbloqueados.forEach( item => {
+    data.sitiosbloqueados.forEach(item => {
         cont++;
         let row = table.insertRow();
         let numero = row.insertCell(0);
-        numero.style.verticalAlign='middle';
+        numero.style.verticalAlign = 'middle';
         numero.innerHTML = cont;
         let url = row.insertCell(1);
-        url.style.verticalAlign='middle';
+        url.style.verticalAlign = 'middle';
         url.innerHTML = item;
         let video = row.insertCell(2);
-        video.style.verticalAlign='middle';
+        video.style.verticalAlign = 'middle';
         video.innerHTML = 'Ibai 1';
         let accion = row.insertCell(3);
-        accion.style.verticalAlign='middle';
-        accion.innerHTML = '<button name="btnGuardar" style="display:none;" class="btn btn-success" ><i class="bi bi-check-lg"></i></button> <button name="btnModificar" class="btn btn-outline-secondary"><i class="bi bi-wrench-adjustable"></i></button> <button name="btnEliminar" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>'
+        accion.style.verticalAlign = 'middle';
+        accion.innerHTML = '<button name="btnGuardar" style="display:none;" class="btn btn-success" ><i class="bi bi-check-lg"></i></button> <button name="btnModificar" class="btn btn-outline-secondary"><i class="bi bi-wrench-adjustable"></i></button> <button name="btnEliminar" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>';
 
-        // OnClick event del boton de modificar:
-        accion.childNodes[2].onclick = function(){
-            modificar(accion.parentNode.rowIndex-1);
-        }
+        // OnClick event for the modify button:
+        accion.childNodes[2].onclick = function() {
+            modificar(accion.parentNode.rowIndex - 1);
+        };
 
-        // OnClick event del boton de eliminar:
-        accion.childNodes[4].onclick = function(){
-            eliminar(accion.parentNode.rowIndex-1);
-        }
+        // OnClick event for the delete button:
+        accion.childNodes[4].onclick = function() {
+            eliminar(accion.parentNode.rowIndex - 1);
+        };
 
-        // OnClick event del boton de guardar:
-        accion.childNodes[0].onclick = function(){
-            // Se obtiene el valor del input de la celda de la URL
+        // OnClick event for the save button:
+        accion.childNodes[0].onclick = function() {
+            // Retrieve the value of the URL input cell
             const input_video = url.childNodes[0].value;
-            guardar(input_video,accion.parentNode.rowIndex-1);
-        }
+            guardar(input_video, accion.parentNode.rowIndex - 1);
+        };
         
     });
-
 });
 
-
-
-function crear(videourl){
+function crear(videourl) {
     chrome.storage.sync.get('sitiosbloqueados', function(data) {
         let nuevossitios = data.sitiosbloqueados;
         nuevossitios.push(videourl);
-        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios}, function() {
-        });
+        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios});
     });
 }
 
-function modificar(indice){
+function modificar(indice) {
     const table = document.getElementById("pagestable");
-    const row = table.childNodes[1].childNodes[indice+2];
-    const btnModificar = row.childNodes[3].childNodes[2];
-    const btnGuardar = row.childNodes[3].childNodes[0];
-    const videocell = row.childNodes[1];
+    const row = table.rows[indice+1];
+    const btnModificar = row.cells[3].childNodes[2];
+    const btnGuardar = row.cells[3].childNodes[0];
+    const videocell = row.cells[1];
     
-    const videourl= videocell.innerHTML;
-    videocell.innerHTML='<input type="text" id="inputvideo" value='+videourl+'>';
+    const videourl = videocell.innerHTML;
+    videocell.innerHTML = `<input type="text" id="inputvideo" value="${videourl}">`;
 
-    btnModificar.style.display='none';
-    btnGuardar.style.display='inline-block';
+    btnModificar.style.display = 'none';
+    btnGuardar.style.display = 'inline-block';
 }
 
-function guardar(videourl,indice){
+function guardar(videourl, indice) {
     chrome.storage.sync.get('sitiosbloqueados', function(data) {
         let nuevossitios = data.sitiosbloqueados;
         nuevossitios[indice] = videourl;
-        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios}, function() {
-        });
+        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios});
     });
     location.reload();
 }
 
-function eliminar(indice){
+function eliminar(indice) {
     chrome.storage.sync.get('sitiosbloqueados', function(data) {
         let nuevossitios = data.sitiosbloqueados;
-        nuevossitios.splice(indice,1);
-        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios}, function() {
-        });
+        nuevossitios.splice(indice, 1);
+        chrome.storage.sync.set({'sitiosbloqueados': nuevossitios});
     });
-    
     location.reload();
 }
-
